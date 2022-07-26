52c9ecd15c6bb3a8a4b0e8840b41f4647f03c2fe
Lucio Ruiz Diaz Gonzalez <luciotadeo02@gmail.com>
2022-07-26 20:54:23 -0300

    terminado 26/07

--- js/site.js
+++ js/site.js
@@ -1,59 +1,119 @@
-const cuotas = (cantCuotas, precio) => {
 
-    const interes = (precio / cantCuotas) * 0.1;
+ class Consola {
+    constructor(nombre, precio){
+        this.nombre = nombre;
+        this.precio = parseFloat(precio);
+    }
+    sumarIva(){
+        this.precio = this.precio * 1.21;
+    }
+
+
+
+/*
+    cuotas(cantCuotas, precio){
+        const precioEnCuotas =  precio / cantCuotas;    
+        switch (cantCuotas) {
+            case 3:
+                alert("El precio en 3 cuotas es de $" + precioEnCuotas.toFixed(2));
+                break;
+    
+            case 6:
+                alert("El precio en 6 cuotas es de $" + precioEnCuotas.toFixed(2));
+                break;
+    
+            case 9:
+                alert("El precio en 9 cuotas es de $" + (precioEnCuotas + interes).toFixed(2));
+                break;
+    
+      
+            case 12:
+                alert("El precio en 12 cuotas es de $" + (precioEnCuotas + interes).toFixed(2));
+                break;
+    
+         }
+    }
+*/
+ }
+
+const consolas = [];
+
+let loop = 1;
+while (loop != 0){
+    let precio = parseFloat(prompt("Ingrese precio de la consola que va a guardar\nIngrese 0 para salir.")).toFixed(2);
+    if (precio == 0){
+        alert ("En la consola van a estar guardadas tus consolas de videojuegos.\nNos vemos!"); 
+        loop = 0;
+       }
+    else{
+        if (isNaN(precio)) {
+            alert("Tenes que ingresar un numero");
+            precio = parseInt(prompt("Ingrese precio de la consola que va a guardar\n Ingrese 0 para salir."));
+        } else {
+           let nombre = prompt("Ingresa el nombre de la consola a guardar. Ingresa ESC para salir.");
+           if(nombre == "ESC"){
+               alert("En la consola van a estar guardadas tus consolas de videojuegos.\nNos vemos!")
+               loop = 0;
+           }else{
+               consolas.push(new Consola(nombre, precio));
+            //    Consola.sumarIva();
+           }
+   
+        }
+    }
+}
+console.log(consolas);
+
+
+
+ /*
+ const cuotas = (cantCuotas, precio) => {
+
+    const precioEnCuotas =  precio / cantCuotas;    
+    const interes = precioEnCuotas * 0.1;
 
     switch (cantCuotas) {
         case 3:
-            alert("El precio en 3 cuotas es de $" + precio / cantCuotas);
+            alert("El precio en 3 cuotas es de $" + precioEnCuotas.toFixed(2));
             break;
 
         case 6:
-            alert("El precio en 6 cuotas es de $" + precio / cantCuotas);
+            alert("El precio en 6 cuotas es de $" + precioEnCuotas.toFixed(2));
             break;
 
         case 9:
-            alert("El precio en 9 cuotas es de $" + ((precio / cantCuotas) + interes));
+            alert("El precio en 9 cuotas es de $" + (precioEnCuotas + interes).toFixed(2));
             break;
 
   
         case 12:
-            alert("El precio en 12 cuotas es de $" + ((precio / cantCuotas) + interes));
+            alert("El precio en 12 cuotas es de $" + (precioEnCuotas + interes).toFixed(2));
             break;
 
+     }
+ }
+ */
 
-    }
-}
+
+
+ /*
+ let cantCuotasFunc = () => {
+     cantCuotas = parseInt(prompt("Ingresa 3, 6, 9 o 12 cuotas. 9 y 12 cuotas incluyen interes del 10%"));
+ }
+ */
+
+
+
+
+
 /*
-let cantCuotasFunc = () => {
-    cantCuotas = parseInt(prompt("Ingresa 3, 6, 9 o 12 cuotas. 9 y 12 cuotas incluyen interes del 10%"));
-}
-*/
-let precio = parseInt(prompt("Ingrese precio a pagar. Ingrese 0 para terminar"));
-while (precio != 0) {
-    if (isNaN(precio)) {
-        alert("Tenes que ingresar un precio");
-        precio = parseInt(prompt("Ingrese precio a pagar. Ingrese 0 para terminar"));
-    } else {
-        let cantCuotas = parseInt(prompt("Ingresa 1, 3, 6, 9 o 12 cuotas. 9 y 12 cuotas incluyen interes del 10%. Ingresa 0 para salir."));
-        if (isNaN(cantCuotas)) {
-            alert("No ingresaste un numero");
-        } else {
-            if (cantCuotas == 3 || cantCuotas == 6 || cantCuotas == 9 || cantCuotas == 12) {
-                cuotas(cantCuotas, precio);
+const productos = [
+    { id: 1, producto: "Arroz"},
+    { id: 2, producto: "Fideo"},
+    { id: 3, producto: "Pan"}
+];
 
-            } else {
-                if (cantCuotas == 1) {
-                    alert("Pagaras $" + precio + " en una cuota");
-
-                } else {
-                    if (cantCuotas == 0) {
-                        alert("Nos vemos!");
-                        break;
-                    } else {
-                        alert("Ingresa una cantidad de cuotas apropiadas.");
-                    }
-                }
-            }
-        }
-    }
-}
+for (const producto of productos) {
+    console.log(producto.id);
+    console.log(producto.producto)
+}*/
